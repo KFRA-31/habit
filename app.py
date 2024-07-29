a73639c7a870aedf5dacdda40cba3c21ce2adba8
KFRA-31 <111846149+KFRA-31@users.noreply.github.com>
2024-07-29 20:25:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient(os.environ.get_one("MONGODB_URI"))
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.get_default_database
     
     app.register_blueprint(pages)
